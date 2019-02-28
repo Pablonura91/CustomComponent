0a59e4ce8c3632ba2f5244f9e2e817a96857c301
Pablo Nuñez <pablonura91@gmail.com>
2019-02-28 09:43:35 +0100

    Update MainActivity

--- app/src/main/java/com/example/customcomponent/MainActivity.kt
+++ app/src/main/java/com/example/customcomponent/MainActivity.kt
@@ -2,17 +2,27 @@ package com.example.customcomponent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.customsumrest.MyViewCustom
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), FragmentCustomComponent.clickListenerCustomView {
+    override fun customViewClicked(currentText: String) {
+        if (findViewById<View>(R.id.ConstraintRight) != null) {
+            val fragmentDetails = BlankFragment.newInstance(currentText)
+            supportFragmentManager.beginTransaction().replace(R.id.ConstraintRight, fragmentDetails)
+                .commit()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        val fragmentList = FragmentCustomComponent()
 
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragmentList).commit()
+        val fragmentList = FragmentCustomComponent()
+        if (findViewById<View>(R.id.container) != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragmentList).commit()
+        } else {
+            supportFragmentManager.beginTransaction().replace(R.id.ConstrainLeft, fragmentList).commit()
+        }
     }
 }
