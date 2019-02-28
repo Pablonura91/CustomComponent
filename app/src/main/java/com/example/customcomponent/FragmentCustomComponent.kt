c116d429f61b9d11e622b411c52ec1bca7dcc1f4
Pablo Nuñez <pablonura91@gmail.com>
2019-02-28 09:43:09 +0100

    Update FragmentCustomComponent

--- app/src/main/java/com/example/customcomponent/FragmentCustomComponent.kt
+++ app/src/main/java/com/example/customcomponent/FragmentCustomComponent.kt
@@ -1,6 +1,7 @@
 package com.example.customcomponent
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -22,8 +23,15 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class FragmentCustomComponent : Fragment(),  MyViewCustom.onClickedButtonPlusListener, MyViewCustom.onClickedButtonMinusListener{
+class FragmentCustomComponent : Fragment(), MyViewCustom.onClickedButtonPlusListener,
+    MyViewCustom.onClickedButtonMinusListener {
     var custom: MyViewCustom? = null
+    var currentText: String = "0"
+    lateinit var listenerCustomView: clickListenerCustomView
+
+    interface clickListenerCustomView {
+        fun customViewClicked(currentText: String)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,37 +39,41 @@ class FragmentCustomComponent : Fragment(),  MyViewCustom.onClickedButtonPlusLis
 
 
     ): View? {
-        /*var custom: MyViewCustom = this.activity!!.findViewById(R.id.custom)
-
-        custom.setOnClickedButtonPlusListener(this)
-        ()var buttonMinus: MyViewCustom = activity!!.findViewById(R.id.button_minus)
-        buttonMinus.setOnClickedButtonPlusListener(this)*/
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_custom_component, container, false)
 
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        listenerCustomView = context as clickListenerCustomView
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setListener()
 
         custom = this.activity!!.findViewById(R.id.custom)
 
         custom!!.setOnClickedButtonPlusListener(this)
         custom!!.setOnClickedButtonMinusListener(this)
-        /*var buttonMinus: MyViewCustom = activity!!.findViewById(R.id.button_minus)
-        buttonMinus.setOnClickedButtonPlusListener(this)*/
 
+    }
+
+    private fun setListener() {
+        listenerCustomView.customViewClicked(currentText)
     }
 
     override fun onClickedButtonPlus(source: MyViewCustom, currentText: TextView) {
         Toast.makeText(this.activity, "button plus clicked", Toast.LENGTH_SHORT).show()
         currentText.text = (currentText.text.toString().toInt() + 1).toString()
+        listenerCustomView.customViewClicked(currentText.text.toString())
     }
 
     override fun onClickedButtonMinus(source: MyViewCustom, currentText: TextView) {
         Toast.makeText(this.activity, "button minus clicked", Toast.LENGTH_SHORT).show()
         currentText.text = (currentText.text.toString().toInt() - 1).toString()
+        listenerCustomView.customViewClicked(currentText.text.toString())
     }
 
 }
